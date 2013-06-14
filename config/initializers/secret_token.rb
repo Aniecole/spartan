3fa3080fe12fcf039483fd8fd083cd6a18a63099
Aniecole <tevin.aniecole@gmail.com>
2013-06-14 11:06:00 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spartan::Application.config.secret_token = '416c9029071d4dd60542dad75424bd24a3abe7cacd901cadd962a66b6c12da8c869a2d235a8564f92c9f4b632b9d60e559beddaeb6d25a2d22ed170baf55e116'
